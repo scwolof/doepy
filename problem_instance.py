a0896fd208a2482431fe4a511c7da0e69eba11eb
scwolof <scw.olofsson@gmail.com>
2019-01-10 12:34:28 +0000

    Added problem_instance

---
+++ problem_instance.py
@@ -0,0 +1,77 @@
+
+import numpy as np 
+import tensorflow as tf 
+
+from gpflow import settings
+from gpflow import Param
+from gpflow.models import Model
+from gpflow.decors import name_scope, params_as_tensors, autoflow
+from gpflow.params import Parameterized, ParamList
+from gpflow.transforms import Logistic
+
+class ProblemInstance (Model):
+	def __init__ (self, models, num_steps, u_bounds, div_criterion=None):
+		Model.__init__(self, name='problem')
+		self.models     = models
+		self.num_models = len(models)
+		self.num_steps  = num_steps
+		self.divergence = div_criterion
+
+		self.Du = len(u_bounds)
+		self.U = ParamList([ Param(np.array([np.mean(ub)]*num_steps), 
+							transform=Logistic(ub[0], ub[1]), 
+							dtype=settings.float_type) 
+						for ub in u_bounds])
+
+	@name_scope('likelihood')
+	@params_as_tensors
+	def _build_likelihood (self):
+		# Divergence term
+		D  = 0.
+		# Constraint violation term
+		C  = 0.
+		# Means and variances of predicted states and observations 
+		mX = [ model.x0.astype(np.float) for model in self.models ]
+		sX = [ model.P0.astype(np.float) for model in self.models ]
+		mY = [ None for model in self.models ]
+		sY = [ None for model in self.models ]
+		# Iterate over the control sequence
+		for n in range( self.num_steps ):
+			u = tf.concat( [self.U[j][n] for j in range(self.Du)], axis=0 )
+			u = tf.reshape( u, [self.Du] )
+			for i, model in enumerate( self.models ):
+				mX[i],sX[i] = model._build_predict_x_dist(mX[i],sX[i], u)[:2]
+				mY[i],sY[i] = model._build_predict_y_dist(mX[i],sX[i])
+			D += self._divergence_objective( mY, sY )
+		return C - D
+
+	def _divergence_objective (self, M, S):
+		D = 0
+		for i in range( self.num_models ):
+			mi = M[i][:,None]
+			Si = S[i]
+			for j in range( i+1, self.num_models ):
+				mj = M[j][:,None]
+				Sj = S[j]
+				if self.divergence is None:
+					mij = mi - mj
+					Sij = tf.linalg.inv( Si + Sj )
+					D  += tf.matmul(mij, tf.matmul(Sij, mij), transpose_a=True)
+				else:
+					D  += self.divergence( mi, Si, mj, Sj )
+		return D
+
+
+	@autoflow()
+	def get_U (self):
+		return self._build_U()
+
+	@name_scope('control_sequence')
+	@params_as_tensors
+	def _build_U (self):
+		U = tf.concat([self.U[j] for j in range(self.Du)], axis=0)
+		U = tf.reshape( U, [self.Du, self.num_steps] )
+		return tf.transpose(U)
+
+
+
