299af5d0f87a67de52614c7cb772aaf6b8af63bc
scwolof <scw.olofsson@gmail.com>
2019-01-10 13:28:01 +0000

    Setup and installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parent
+
+about = {}
+version_path = project_root / 'GPdode' / '__version__.py'
+with version_path.open() as f:
+    exec(f.read(), about)
+
+setup(
+    name='GPdode',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    license=about['__license__'],
+    version=about['__version__'],
+    url='https://github.com/scwolof/GPdode',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['numpy>=1.7', 'gpflow>=1.2,<2.0'],
+    setup_requires=['numpy>=1.7', 'gpflow>=1.2,<2.0'],
+    tests_require=['pytest-runner','pytest', 'pytest-cov'],
+)
