8e1dc3ba1b73662a558bf9bfbbe7760834139796
scwolof <scw.olofsson@gmail.com>
2019-01-14 17:37:30 +0000

    Added control inputs

--- GPdode/case_studies/SkandaLebiedz2010.py
+++ GPdode/case_studies/SkandaLebiedz2010.py
@@ -7,34 +7,45 @@ D. Skanda and D. Lebiedz (2010)
 "An optimal experimental design approach to model discrimination
 in dynamic biochemical systems"
 Bioinformatics 26(7): 939-945
+
+D. Skanda and D. Lebiedz (2013)
+""
+
+Comments:
+Original case study does not have control inputs.
+The experimental design is wrt to measurement time points.
+We modify this by introducing controls u1 and u2 that perturb the states.
 """
 
 class Model:
 	def __init__ (self):
 		self.num_states   = 2
-		#self.num_controls = 2
+		self.num_controls = 2
 		self.num_measured = 2
 
 		self.H = np.eye(2)        # Observation matrix
 		self.Q = np.zeros((2,2))  # Process noise covariance
 		self.R = np.eye(2)        # Measurement noise covariance
 
-		self.x0 = np.array([ 14.7, 2.21 ])
+		self.x0 = np.array([ 15., 2. ])
+		self.x0_bounds = np.array([[1e-7, 25],[1e-7, 25]])
 
 		#self.T  = 72.0
 		#self.dt = 0.75
 		#self.num_steps = 97
 
-		#self.u_bounds = np.array([[0.05, 0.2], [5., 35.]])
+		self.u_bounds = np.array([[0., 0.2], [0., 0.2]])
 
 	def __call__ (self, x, u, p):
 		"""
 		Inputs:
-		   x   States at time t
-		   u   Controls at time t
-		   p   Model parameters
+		   x    States at time t:
+		        x[0] - substrate concentration
+		        x[1] - product concentration
+		   u    Controls at time t
+		   p    Model parameters
 		Outputs:
-		   dx  States at time t+1
+		   dx   States at time t+1
 		"""
 		f = lambda y,t: self._ode_func(y,u,p)
 		#t = np.linspace(0, self.dt, 26)
@@ -49,16 +60,17 @@ class M1 (Model):
 	def __init__ (self):
 		Model.__init__(self)
 		self.p0 = np.array([ 0.879, 2.03, 0.109, 1.61e4 ])
+		self.p_bounds = np.array([[1e-7, 10],[1e-7,10],[1e-7,10],[1e3,1e5]])
 		self.num_param = len( self.p0 )
 
 	def _ode_func (self, x, u, p):
 		x1, x2 = x
-		#u1, u2 = u
+		u1, u2 = u
 		p1, p2, p3, p4 = p 
 
 		r   = ( x1 * (1+x1) * (1+x2)**2 ) / ( p4 + (1+x1)**2 * (1+x2)**2 )
-		dx1 = 0.22 - p1 * r
-		dx2 = (p2 * p1 * r) - (p3 * x2)
+		dx1 = 0.22 - p1 * r + u1
+		dx2 = (p2 * p1 * r) - (p3 * x2) + u2
 		return np.array([dx1, dx2])
 
 
@@ -70,16 +82,17 @@ class M2 (Model):
 	def __init__ (self):
 		Model.__init__(self)
 		self.p0 = np.array([ 6.47, 3.33, 5.11, 244 ])
+		self.p_bounds = np.array([[1e-7, 100],[1e-7,100],[1e-7,100],[100,300]])
 		self.num_param = len( self.p0 )
 
 	def _ode_func (self, x, u, p):
 		x1, x2 = x
-		#u1, u2 = u
+		u1, u2 = u
 		p1, p2, p3, p4 = p 
 
 		r   = ( x1 * (1+x2) ) / ( p4 + (1+x1) * (1+x2) )
-		dx1 = 0.22 - r
-		dx2 = p1 * r - (p2 * x2) / (p3 + x2)
+		dx1 = 0.22 - r + u1
+		dx2 = p1 * r - (p2 * x2) / (p3 + x2) + u2
 		return np.array([dx1, dx2])
 
 
