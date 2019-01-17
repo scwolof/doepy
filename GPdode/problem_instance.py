1a6fb59021f5d8fa48c242a91d216636998d7183
scwolof <scw.olofsson@gmail.com>
2019-01-17 11:53:13 +0000

    Model -> Parameterized

--- GPdode/problem_instance.py
+++ GPdode/problem_instance.py
@@ -11,9 +11,13 @@ from gpflow.transforms import Logistic
 
 from .design_criteria import HR
 
-class ProblemInstance (Model):
-	def __init__ (self, models, num_steps, u_bounds, div_criterion=None):
-		Model.__init__(self, name='problem')
+class ProblemInstance (Parameterized):
+	def __init__ (self, models, num_steps, u_bounds, y_bounds, 
+			y_constraint_prob=0.99, div_criterion=None):
+		super(ProblemInstance, self).__init__(name='problem')
+		self._objective = None
+		self._likelihood_tensor = None
+
 		self.models     = models
 		self.num_models = len(models)
 		self.num_steps  = num_steps
@@ -25,16 +29,80 @@ class ProblemInstance (Model):
 							dtype=settings.float_type) 
 						for ub in u_bounds])
 
-	@name_scope('likelihood')
+		self.y_bounds = y_bounds
+		self.y_const_prob = y_constraint_prob
+		assert isinstance(self.y_const_prob, float) and 0<self.y_const_prob<1
+
+	@property
+	def objective(self):
+		return self._objective
+
+	@property
+	def likelihood_tensor(self):
+		return self._likelihood_tensor
+
+	@autoflow()
+	def compute_log_prior(self):
+		"""Compute the log prior of the model."""
+		return self.prior_tensor
+
+	@autoflow()
+	def compute_log_likelihood(self):
+		"""Compute the log likelihood of the model."""
+		return self.likelihood_tensor
+
+	def is_built(self, graph):
+		is_built = super().is_built(graph)
+		if is_built is not Build.YES:
+			return is_built
+		if self._likelihood_tensor is None:
+			return Build.NO
+		return Build.YES
+
+	def build_objective(self):
+		likelihood = self._build_objective()
+		priors = []
+		for param in self.parameters:
+			unconstrained = param.unconstrained_tensor
+			constrained = param._build_constrained(unconstrained)
+			priors.append(param._build_prior(unconstrained, constrained))
+		prior = self._build_prior(priors)
+		return -tf.add(likelihood, prior, name='objective')
+
+	def _clear(self):
+		super(Model, self)._clear()
+		self._likelihood_tensor = None
+		self._objective = None
+
+	def _build(self):
+		super(Model, self)._build()
+		likelihood = self._build_objective()
+		prior = self.prior_tensor
+		#objective = self._build_objective(likelihood, prior)
+		self._likelihood_tensor = likelihood
+		self._objective = -tf.add(likelihood, prior, name='objective')
+
+	"""
+	def sample_feed_dict(self, sample):
+		tensor_feed_dict = {}
+		for param in self.parameters:
+			if not param.trainable: continue
+			constrained_value = sample[param.pathname]
+			unconstrained_value = param.transform.backward(constrained_value)
+			tensor = param.unconstrained_tensor
+			tensor_feed_dict[tensor] = unconstrained_value
+		return tensor_feed_dict
+	"""
+
+	#def _build_objective(self, likelihood_tensor, prior_tensor):
+	#    func = tf.add(likelihood_tensor, prior_tensor, name='nonneg_objective')
+	#    return tf.negative(func, name='objective')
+
+	@name_scope('objective')
 	@params_as_tensors
-	def _build_likelihood (self):
+	def _build_objective (self):
 		# Divergence term
 		D  = 0.
-		# Constraint violation term
-		#C  = 0.
-		# Control delta constraint
-		#Udelta = 0.
-		#u_old  = None
 		# Means and variances of predicted states and observations 
 		mX = [ model.x0.astype(np.float) for model in self.models ]
 		sX = [ model.P0.astype(np.float) for model in self.models ]
@@ -45,23 +113,23 @@ class ProblemInstance (Model):
 			# Next control input
 			u = tf.concat( [self.U[j][n] for j in range(self.Du)], axis=0 )
 			u = tf.reshape( u, [self.Du] )
-			#if u_old is None:
-			#	u_old = u
 			# State prediction
 			for i, model in enumerate( self.models ):
 				mX[i],sX[i] = model._build_predict_x_dist(mX[i],sX[i], u)[:2]
 				mY[i],sY[i] = model._build_predict_y_dist(mX[i],sX[i])
 			D += self.divergence( mY, sY )
-		#return C - D
 		return -D
 
-	@autoflow()
-	def get_U (self):
-		return self._build_U()
+	def _build_constraints (self, mY, sY):
+		return NotImplementedError
 
-	@name_scope('control_sequence')
+	@autoflow()
+	def control_signal (self):
+		return self._build_control_signal()
+
+	@name_scope('control_signal')
 	@params_as_tensors
-	def _build_U (self):
+	def _build_control_signal (self):
 		U = tf.concat([self.U[j] for j in range(self.Du)], axis=0)
 		U = tf.reshape( U, [self.Du, self.num_steps] )
 		return tf.transpose(U)
