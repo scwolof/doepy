f05315fe5d198d9fcd2982a6d5bcedc48cfbd528
scwolof <scw.olofsson@gmail.com>
2019-01-18 10:01:56 +0000

    Constrained optimisers - tf.contrib fork

---
+++ GPdode/training/tf_contrib_constrained_optimization_forked/__init__.py
@@ -0,0 +1,7 @@
+
+
+from .candidates import *
+from .constrained_minimization_problem import *
+from .constrained_optimizer import *
+from .external_regret_optimizer import *
+from .swap_regret_optimizer import *
