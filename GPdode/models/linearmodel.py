d8f7506a5aa05685ad9b75e24c742679451bbef2
scwolof <scw.olofsson@gmail.com>
2019-01-22 14:07:06 +0000

    Added predict x dist gradients

--- GPdode/models/linearmodel.py
+++ GPdode/models/linearmodel.py
@@ -14,7 +14,7 @@ class LinearModel (Model):
 
 		Model:
 			x_{k+1} = F * x_k  +  B * u_k  +  w_k,   w_k ~ N(0, Q)
-			    y_k = H * x_k  +  v_k,               v_k ~ N(0, R)
+				y_k = H * x_k  +  v_k,               v_k ~ N(0, R)
 		"""
 		f = lambda x,u: np.matmul(F,x) + np.matmul(B,u)
 		super(LinearModel, self).__init__(f, H, Q, R)
@@ -56,8 +56,29 @@ class LinearModel (Model):
 	"""
 	State prediction
 	"""
-	def _predict_x_dist (self, xk, Pk, u, cross_cov=False):
+	def _predict_x_dist (self, xk, Sk, u, cross_cov=False, grad=False):
 		mk = np.matmul(self.F, xk) + np.matmul(self.B, u)
-		Vk = np.matmul(Pk, self.F.T)
+		Vk = np.matmul(Sk, self.F.T)
 		Sk = np.matmul(self.F, Vk) + self.Q
-		return (mk, Sk, Vk) if cross_cov else (mk, Sk)
+		if not grad:
+			return (mk, Sk, Vk) if cross_cov else (mk, Sk)
+		# Compute gradients
+		D, Du = self.B.shape
+		dXdx = self.F
+		dXds = np.zeros(( self.D, self.D, self.D ))
+		dXdu = self.B
+		dSdx = np.zeros(( self.D, self.D, self.D ))
+		dSds = np.zeros(( self.D, self.D, self.D, self.D ))
+		for d1 in range( self.D ):
+			for d2 in range( self.D ):
+				dSds[d1,d2] = self.F[d1][:,None] * self.F[d2][None,:]
+		dSdu = np.zeros(( self.D, self.D, self.Du ))
+		if not cross_cov:
+			return mk, Sk, dXdx, dXds, dXdu, dSdx, dSds, dSdu
+		# Compute cross-covariance
+		dVdx = np.zeros(( self.D, self.D, self.D ))
+		dVds = np.zeros(( self.D, self.D, self.D, self.D ))
+		for d1 in range( self.D ):
+			dVds[d1,:,d1] = self.F
+		dVdu = np.zeros(( self.D, self.D, self.Du ))
+		return mk, Sk, Vk, dXdx, dXds, dXdu, dSdx, dSds, dSdu, dVdx, dVds, dVdu
