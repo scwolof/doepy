a93a4acbba40ea075571448fd74f0852ab116134
scwolof <scw.olofsson@gmail.com>
2019-01-22 11:04:46 +0000

    Removed tensorflow optimizers

--- GPdode/training/tf_contrib_constrained_optimization_forked/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-from .candidates import *
-from .constrained_minimization_problem import *
-from .constrained_optimizer import *
-from .external_regret_optimizer import *
-from .swap_regret_optimizer import *
