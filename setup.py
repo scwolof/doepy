f3d8f3ffa23467b645b4e405d7e823d531eb6c32
scwolof <scw.olofsson@gmail.com>
2019-02-12 13:37:59 +0000

    Name GPdode -> doepy

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from pathlib import Path
 project_root = Path(__file__).resolve().parent
 
 about = {}
-version_path = project_root / 'GPdode' / '__version__.py'
+version_path = project_root / 'doepy' / '__version__.py'
 with version_path.open() as f:
     exec(f.read(), about)
 
 setup(
-    name='GPdode',
+    name='doepy',
     author=about['__author__'],
     author_email=about['__author_email__'],
     license=about['__license__'],
     version=about['__version__'],
-    url='https://github.com/scwolof/GPdode',
+    url='https://github.com/scwolof/doepy',
     packages=find_packages(exclude=['tests']),
     install_requires=['numpy>=1.7', 'GPy'],
     setup_requires=['numpy>=1.7', 'GPy'],
