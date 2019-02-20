f0d53b0b96be635092c41be11fd79e85420b63bd
scwolof <scw.olofsson@gmail.com>
2019-02-20 13:53:24 +0000

    Added discrete-time case study

---
+++ doepy/case_studies/discrete_time/cs1.py
@@ -0,0 +1,168 @@
+"""
+MIT License
+
+Copyright (c) 2019 Simon Olofsson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import numpy as np 
+
+class Model:
+	def __init__ (self, num_states):
+		self.num_states = num_states
+		self.num_inputs = 2
+		self.num_meas   = 2
+
+		self.H = np.zeros((self.num_meas, self.num_states))
+		self.H[0,0], self.H[1,2] = 1, 1
+
+		self.R  = 2.5e-3 * np.eye( self.num_meas )
+		self.Q  = 1e-4 * np.eye( self.num_states )
+		self.Su = 1e-5 * np.eye( self.num_inpus )
+
+		self.x0 = np.zeros( self.num_states )
+		self.P0 = 1e-6 * np.eye( self.num_states )
+
+		self.hessian = True
+
+
+class M1 (Model):
+	def __init__ (self, x, u, grad=True):
+		super().__init__(3)
+
+	def __call__ (x, u, grad=False):
+		x1, x2, x3 = x
+		u1, u2     = u
+		xk1 = u1
+		xk2 = x3
+		xk3 = x1 + x2 * u2
+		g   = np.array([xk1, xk2, xk3])
+		if not grad:
+			return g
+		# dgdx
+		dgdx      = np.zeros((3,3))
+		dgdx[1,2] = 1
+		dgdx[2]   = np.array([1., u2, 0])
+		# dgdu
+		dgdu      = np.zeros((3,2))
+		dgdu[0,0] = 1
+		dgdu[2]   = np.array([0., x2])
+		# ddgddx
+		ddgddx = np.zeros((3,3,3))
+		ddgddu = np.zeros((3,2,2))
+		ddgdxu = np.zeros((3,3,2))
+		ddgdxu[2,1,1] = 1
+		return g, dgdx, dgdu, ddgddx, ddgddu, ddgdxu
+
+
+class M2 (Model):
+	def __init__ (self, x, u, grad=True):
+		super().__init__(3)
+
+	def __call__ (x, u, grad=False):
+		x1, x2, x3 = x
+		u1, u2     = u
+		xk1 = u1
+		xk2 = x3 + (x1 - u1)
+		xk3 = x1 + x2 * u2
+		g = np.array([xk1, xk2, xk3])
+		if not grad:
+			return g
+		# dgdx
+		dgdx      = np.zeros((3,3))
+		dgdx[1]   = np.array([1., 0., 1.])
+		dgdx[2]   = np.array([1., u2, 0])
+		# dgdu
+		dgdu = np.array([[1.,0],[-1,0.],[0.,x2]])
+		# ddgddx
+		ddgddx = np.zeros((3,3,3))
+		ddgddu = np.zeros((3,2,2))
+		ddgdxu = np.zeros((3,3,2))
+		ddgdxu[2,1,1] = 1
+		return g, dgdx, dgdu, ddgddx, ddgddu, ddgdxu
+
+
+class M1 (Model):
+	def __init__ (self, x, u, grad=True):
+		super().__init__(4)
+
+	def __call__ (x, u, grad=False):
+		x1, x2, x3, x4 = x
+		u1, u2 = u
+		xk1 = u1
+		xk2 = x3 + x4
+		xk3 = x1 + x2 * u2
+		xk4 = x1 - u1
+		g = np.array([xk1, xk2, xk3, xk4])
+		if not grad:
+			return g
+		# dgdx
+		dgdx    = np.zeros((4,4))
+		dgdx[1] = np.array([0., 0., 1., 1.])
+		dgdx[2] = np.array([1., u2, 0., 0.])
+		dgdx[3] = np.array([1., 0., 0., 0.])
+		# dgdu
+		dgdu = np.array([[1.,0],[0.,0.],[0.,x2],[-1.,0.]])
+		# ddgddx
+		ddgddx = np.zeros((4,4,4))
+		ddgddu = np.zeros((4,2,2))
+		ddgdxu = np.zeros((4,4,2))
+		ddgdxu[2,1,1] = 1
+		return g, dgdx, dgdu, ddgddx, ddgddu, ddgdxu
+
+
+class M4 (Model):
+	def __init__ (self, x, u, grad=True):
+		super().__init__(4)
+
+	def __call__ (x, u, grad=False):
+		x1, x2, x3, x4 = x
+		u1, u2 = u
+		xk1 = u1
+		xk2 = x3 + x4
+		xk3 = x1 + x2 * u2
+		xk4 = x1 * (x1 - u1)
+		g = np.array([xk1, xk2, xk3, xk4])
+		if not grad:
+			return g
+		# dgdx
+		dgdx    = np.zeros((4,4))
+		dgdx[1] = np.array([0., 0., 1., 1.])
+		dgdx[2] = np.array([1., u2, 0., 0.])
+		dgdx[3] = np.array([2.*x1 - u1, 0., 0., 0.])
+		# dgdu
+		dgdu = np.array([[1.,0],[0.,0.],[0.,x2],[-x1,0.]])
+		# ddgddx
+		ddgddx = np.zeros((4,4,4))
+		ddgddx[3,0,0] = 2.
+		ddgddu = np.zeros((4,2,2))
+		ddgdxu = np.zeros((4,4,2))
+		ddgdxu[2,1,1] =  1.
+		ddgdxu[3,0,0] = -1.
+		
+		return g, dgdx, dgdu, ddgddx, ddgddu, ddgdxu
+
+
+class DataGen (M1):
+	def __init__ (self):
+		super().__init__()
+
+def get ():
+	return DataGen(), [M1(), M2(), M3(), M4()]
