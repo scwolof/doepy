828df4a80e5891ee5cd2797320b95f24779ec29d
scwolof <scw.olofsson@gmail.com>
2019-03-07 17:27:22 +0000

    Brute force feasibility analysis

--- doepy/feasibility/__init__.py
+++ doepy/feasibility/__init__.py
@@ -3,3 +3,4 @@ from .bound_updates import (stretch_bounds, BoundUpdate, EmpiricalBoxBoundUpdate
 	MaximumProbableBoundUpdate)
 from .bo_acquisition_functions import expected_improvement
 from .bo_feasibility_analysis import bayesian_optimisation
+from .sample_feasibility_analysis import sample_optimisation
