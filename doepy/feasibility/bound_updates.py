3f397a4b18adde3d4e7a4b1afc44aa90bc65a582
scwolof <scw.olofsson@gmail.com>
2019-03-07 13:11:21 +0000

    Take dict input, return X, y

--- doepy/feasibility/bound_updates.py
+++ doepy/feasibility/bound_updates.py
@@ -23,6 +23,7 @@ SOFTWARE.
 """
 
 import numpy as np
+import warnings
 
 
 def stretch_bounds (bounds, alpha=0.2):
@@ -32,46 +33,60 @@ def stretch_bounds (bounds, alpha=0.2):
 
 
 class BoundUpdate:
-	def __init__ (self, known_dimensions):
+	def __init__ (self, known_dimensions=[], remove_data=True):
 		"""
 		known_dimensions - list with indices for dimensions with known bounds
 		                   (these bounds should not be modified!)
 		"""
-		self.dim = known_dimensions
+		self.known_dim = known_dimensions
+		self.rm_data   = remove_data
 
-	def __call__ (self, bounds, X, Y, i=None, remove_data=True):
+	def __call__ (self, bounds, **kwargs):
 		# Update bounds
-		bounds = self._update_unknown_dims(bounds, X, Y, i=i)
+		bounds, X, y = self._update_unknown_dims(bounds, **kwargs)
 
 		# Remove data outside box
-		if not remove_data:
-			return bounds, X, Y
-		I = [i for i,x in enumerate(X) \
-		        if  np.all( x >= bounds[:,0] ) and np.all( x <= bounds[:,1] )]
-		return bounds, X[I], Y[I]
+		if self.rm_data:
+			I = [i for i,x in enumerate(X) \
+				if  np.all( x >= bounds[:,0] ) and np.all( x <= bounds[:,1] )]
+			X, y = X[I], y[I]
+		return bounds, X, y
 
-	def _update_unknown_dims (self, bounds, X, Y, i=None):
-		new_bounds = self._update(bounds.copy(), X, Y, i=i)
-		for d in self.dim:
+	def _update_unknown_dims (self, bounds, **kwargs):
+		new_bounds, X, y = self._update(bounds.copy(), **kwargs)
+		for d in self.known_dim:
 			new_bounds[d] = bounds[d]
-		return new_bounds
+		return new_bounds, X, y
 
 
 class EmpiricalBoxBoundUpdate (BoundUpdate):
-	def __init__ (self, known_dimensions, stretch=0.5):
-		super().__init__(known_dimensions)
+	"""
+	Return smallest box bounding all observed feasible points
+	"""
+	def __init__ (self, *args, stretch=0.5, **kwargs):
+		super().__init__(*args, **kwargs)
 		assert isinstance(stretch, (int,float)) and stretch >= 0
 		self.stretch = stretch
 
-	def _update (self, bounds, X, Y, i=None):
+	def _update (self, bounds, **kwargs):
+		if ('X' in kwargs and 'Y' in kwargs):
+			X, y  = kwargs.get('X'), kwargs.get('Y')
+		elif not 'model' in kwargs:
+			raise ValueError('Need data to update empirical box bounds')
+		else:
+			model = kwargs.get('model')
+			X, y  = model.X.copy(), model.Y[:,0].copy()
+		if y.ndim > 1:
+			y = y.flatten()
+		assert X.shape[0] == len(y), 'Data not of equal length!'
+
 		# Find feasible observations
-		I = np.arange(len(Y))[Y>=0]
-		if len(I) <= 2*len(bounds):
-			return bounds
-		Xf = X[I]
-		# Stretched empirical box
-		for d in range( bounds.shape[0] ):
-			if d in self.dim: continue
-			bounds[d] = np.array([ np.min(Xf[:,d]), np.max(Xf[:,d]) ])
-			bounds[d] = stretch_bounds(bounds[d], self.stretch)
-		return bounds
+		I = np.arange(len(y))[y>=0]
+		if len(I) > 2*len(bounds):
+			# Stretched empirical box
+			Xf = X[I]
+			for d in range( bounds.shape[0] ):
+				if d in self.known_dim: continue
+				bounds[d] = np.array([ np.min(Xf[:,d]), np.max(Xf[:,d]) ])
+				bounds[d] = stretch_bounds(bounds[d], self.stretch)
+		return bounds, X, y
