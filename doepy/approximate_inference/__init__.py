df5e0595a69e21edba85266df1a1d71bc35ccde9
scwolof <scw.olofsson@gmail.com>
2019-03-14 11:05:15 +0000

    Import new functions

--- doepy/approximate_inference/__init__.py
+++ doepy/approximate_inference/__init__.py
@@ -1,2 +1,3 @@
-
-from .rbf_moment_match import rbf_moment_match
+from .exact_moment_match import exact_moment_match
+from .taylor_moment_match import taylor_moment_match
+from .gp_taylor_moment_match import gp_taylor_moment_match
