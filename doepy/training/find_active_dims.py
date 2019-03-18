a0cf3670d4f718dd28f5574882a58761db817816
scwolof <scw.olofsson@gmail.com>
2019-03-18 15:17:18 +0000

    Find active dims from [x,u] or [x,u,p]

--- doepy/training/find_active_dims.py
+++ doepy/training/find_active_dims.py
@@ -24,36 +24,47 @@ SOFTWARE.
 
 import numpy as np
 
-def find_active_dims (f, xbs, ubs, num_points=5, threshold=1e-10):
+def find_active_dims (f, bounds_list, num_points=5, threshold=1e-10,
+                      return_data=False):
 	"""
-	xbs - x bounds
-	ubs - u bounds
+	bounds_list = [ xbs, ubs, (pbs) ]
+	xbs - latent state bounds
+	ubs - control input bounds
+	pbs - model parameter bounds (optional)
+
+	The bounds lists have to be in the order that the parameters are given
+	as inputs to the function f, i.e.
+	bounds_list = [xbs, ubs]       =>  y = f(x, u)
+	bounds_list = [xbs, ubs, pbs]  =>  y = f(x, u, p)
 	"""
-	D = xbs.shape[0]
-	B = ubs.shape[0]
+	Ds   = [ b.shape[0] for b in bounds_list ]
+	Dss  = np.cumsum([0] + Ds).tolist()
+	Dtot = np.sum( Ds )
+	bnds = np.vstack(bounds_list)
+	assert bnds.shape[1] == 2
+
+	def stack_linspace (bounds):
+		return np.vstack([ np.linspace(*b, num_points) for b in bounds ])
+	X = np.vstack([ stack_linspace(b) for b in bounds_list ]).T
+
+	def separate (x):
+		return [ x[di:dj] for di,dj in zip(Dss, Dss[1:]) ]
 	
-	assert xbs.shape == (D, 2)
-	X = [ np.linspace(*xb, num_points) for xb in xbs ]
-	X = np.vstack([x for x in X]).T
+	# num_points-1 model evaluations
+	Y = np.array([ f(*separate(x)) for x in X[:-1] ])
+	E = Y.shape[1]
 	
-	assert ubs.shape == (B, 2)
-	U = [ np.linspace(*ub, num_points) for ub in ubs ]
-	U = np.vstack([u for u in U]).T
-	
-	# N-1 model evaluations
-	Y = np.array([ f(X[n],U[n]) for n in range(num_points-1) ])
-	
-	T  = np.c_[X, U]
-	Di = np.zeros((D+B, D))
-	# (N-1)*(D+B) model evaluations
-	for n in range(num_points-1):
-		for d1 in range(D+B):
-			t       = T[n].copy()
-			t [d1]  = T[n+1,d1]
-			y       = f(t[:D], t[D:])
-			Y, T    = np.vstack((Y, y)), np.vstack((T, t))
-			Di[d1] += np.abs(Y[n] - y)
+	Di = np.zeros((Dtot, E))
+	# (num_points-1)*Dtot model evaluations
+	for n in range( num_points-1 ):
+		for d in range( Dtot ):
+			x      = X[n].copy()
+			x[d]   = X[n+1,d]
+			y      = f(*separate(x))
+			Y, X   = np.vstack((Y, y)), np.vstack((X, x))
+			Di[d] += np.abs(Y[n] - y)
 	
 	C = Di.T >= threshold
-	# Total (N-1)*(1+D+B) model evaluations
-	return [ [i for i in np.arange(D+B) if C[d,i]] for d in range(D) ], Y, T
+	active_dims = [ [i for i in np.arange(Dtot) if C[e,i]] for e in range(E) ]
+	# Total (num_points-1)*(1+Dtot) model evaluations
+	return (active_dims, X, Y) if return_data else active_dims
