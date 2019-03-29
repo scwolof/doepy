de035f81c21ba91e295ee861b615a6d29eed3d26
scwolof <scw.olofsson@gmail.com>
2019-03-29 16:08:16 +0000

    Updated case study

--- doepy/case_studies/discrete_time/nonlinear2.py
+++ doepy/case_studies/discrete_time/nonlinear2.py
@@ -27,6 +27,7 @@ from scipy.stats import norm
 
 class Model:
     def __init__ (self, name, num_states):
+        self.name = name
 
         self.num_inputs = 1
         self.num_states = num_states
@@ -43,6 +44,8 @@ class Model:
         self.R   = 2e-4 * np.eye(self.num_meas)
         self.S_u = 1e-5 * np.eye(self.num_inputs)
         
+        self.p0 = [0.2, 0.1, 0.01]
+
         self.x0    = np.zeros(self.num_states)
         self.x0[0] = 1
         self.x0[2] = 0.01
@@ -51,36 +54,43 @@ class Model:
         self.S_x0[0,0] = 1e-4
         self.S_x0[2,2] = 1e-6
         
-        self.u_bounds = np.array([[0., 1.]])
-        self.x_bounds = np.array([[0,2],[0,5],[0,5]])
+        self.u_bounds = np.array([[0., 0.1]])
+        self.x_bounds = np.array([[0.,2.],[0.,3.],[0.,5.]])
 
     def __call__ (self, x, u, p):
         dx = x + self.change(x, u, p)
         dx = np.maximum(dx, 0)
         return dx
 
-	def get_candidate_dict (self):
-		return {'f':  self,
-		        'H':  self.H,
-		        'x0': self.x0,
-		        'name': self.name,
-		        'x_covar': self.Q,
-		        'y_covar': self.R,
-		        'u_covar': self.S_u,
-		        'x0_covar': self.S_x0,
-		        'hessian':  False,
-		        'x_bounds': self.x_bounds,
-		        'u_bounds': self.u_bounds,
-		        'num_meas': self.num_meas,
-		        'num_param': len(self.p0)
-		        'num_inputs': self.num_inputs,
-		        'delta_transition': True}
+    @property
+    def num_param (self):
+        return len( self.p0 )
+
+    @property
+    def p_bounds (self):
+        return np.array([[1e-8, 1.]] * self.num_param)
+
+    def get_candidate_dict (self):
+        return {'f':  self,
+                'H':  self.H,
+                'x0': self.x0,
+                'name': self.name,
+                'x_covar': self.Q,
+                'y_covar': self.R,
+                'u_covar': self.S_u,
+                'hessian':  False,
+                'x0_covar': self.S_x0,
+                'x_bounds': self.x_bounds,
+                'u_bounds': self.u_bounds,
+                'num_meas': self.num_meas,
+                'num_param': self.num_param,
+                'num_inputs': self.num_inputs,
+                'delta_transition': True}
 
 
 class M1 (Model):
     def __init__ (self):
         super().__init__('M1',3)
-        self.p0 = [0.2, 0.2, 0.01]
 
     def change (self, x, u, p):
         S, A, V    = x
@@ -93,7 +103,7 @@ class M1 (Model):
 class M2 (Model):
     def __init__ (self):
         super().__init__('M2',3)
-        self.p0 = [0.2, 0.2, 0.01, 0.05, 0.01]
+        self.p0 += [0.05, 0.01]
 
     def change (self, x, u, p):
         S, A, V            = x
@@ -107,7 +117,7 @@ class M2 (Model):
 class M3 (Model):
     def __init__ (self):
         super().__init__('M3',4)
-        self.p0 = [0.2, 0.2, 0.01, 0.05, 0.01, 0.01]
+        self.p0 += [0.05, 0.01, 0.01]
         self.x_bounds = np.vstack((self.x_bounds, np.array([0., 1.])))
 
     def change (self, x, u, p):
@@ -122,12 +132,20 @@ class M3 (Model):
 
 
 class DataGen (M3):
-	def __init__ (self):
-		super().__init__()
-		self.p0 = [0.201, 0.198, 0.0099, 0.051, 0.0101, 0.0098]
+    def __init__ (self):
+        super().__init__()
 
-	def __call__ (self, x, u):
-		return super().__call__(x, u, self.p0)
+    @property
+    def p (self):
+        return [0.201, 0.098, 0.0099, 0.051, 0.0101, 0.0098]
+
+    def __call__ (self, x, u):
+        return super().__call__(x, u, self.p)
+
+    def get_candidate_dict (self):
+        d = super().get_candidate_dict()
+        d['num_param'] = 0
+        return d
     
 def get ():
     return DataGen(), [M1(), M2(), M3()]
