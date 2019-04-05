b4dcb6d0da9c2d998700d876f48a1f295a14ec27
scwolof <scw.olofsson@gmail.com>
2019-04-05 14:10:29 +0100

    Aesthetic change

--- doepy/model_discrimination/__init__.py
+++ doepy/model_discrimination/__init__.py
@@ -1,7 +1,3 @@
-
-#from .problem_instance import ProblemInstance
-#from .design_criteria import ...
-
 from .chi_squared_test import chi2_adequacy_test
 from .marginal_likelihood import relative_likelihood
 from .KL_divergence import symmetric_KL
