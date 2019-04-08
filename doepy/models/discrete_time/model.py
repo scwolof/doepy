ccbe371348bfdd672649dd22813a3c3492a10632
scwolof <scw.olofsson@gmail.com>
2019-04-08 17:03:39 +0100

    Moved predict function calls

--- doepy/models/discrete_time/model.py
+++ doepy/models/discrete_time/model.py
@@ -25,10 +25,7 @@ SOFTWARE.
 import numpy as np 
 from numpy.random import multivariate_normal as mvn
 
-from . import LatentStateDerivativeObject, MeasDerivativeObject
-
 from ..statespacemodel import StateSpaceModel
-from ...utils import assert_symmetric_matrix, assert_is_shape
 
 class dtModel (StateSpaceModel):
 	def __init__ (self, candidate_model):
@@ -48,164 +45,15 @@ class dtModel (StateSpaceModel):
 	"""
 	Function calls
 	"""
-	def predict (self, x0, U):
-		"""
-		Deterministic prediction from model
-			x_{k+1} = f( x_k, u_k )
-			    y_k = H * x_k
-
-		If U.ndim == 1, one-step prediction
-		If U.ndim == 2, multi-step prediction
-		"""
-		if x0 is None:
-			x0 = self.x0
-
-		if U.ndim == 1:
-			return self._predict(x0, U)
-
-		n = len(U)
-		X = np.zeros(( n+1, self.num_states ))
-		Y = np.zeros((   n, self.num_meas ))
-
-		X[0] = x0
-		for k in range( n ):
-			X[k+1], Y[k] = self._predict(X[k], U[k])
-		return X, Y
-
 	def _predict (self, x, u):
-		args = (x, u) if self.num_param <= 0 else (x, u, self.p_mean)
+		args = (x, u) if self.num_param < 1 else (x, u, self.p_mean)
 		xk1  = self.f(*args)
 		yk   = np.matmul(self.H, x)
 		return xk1, yk
 
-	def sample (self, x0, U, initial_uncertainty=False):
-		"""
-		Stochastic model simulation
-			x_{k+1} = f( x_k, u_k ) + w_k
-			    y_k = H * x_k + v_k
-
-		If U.ndim == 1, one-step prediction
-		If U.ndim == 2, multi-step prediction
-		"""
-		if x0 is None:
-			x0 = self.x0
-		x0 = x0 if not initial_uncertainty else mvn(x0, self.x0_covar)
-
-		if U.ndim == 1:
-			return self._sample(x0, U)
-
-		n = len(U)
-		X = np.zeros(( n+1, self.num_states ))
-		Y = np.zeros((   n, self.num_meas ))
-
-		X[0] = x0
-		for k in range(n):
-			X[k+1], Y[k] = self._sample(X[k], U[k])
-		return X, Y
-
-	def _sample (self, x, u):
-		us = mvn(u, self.u_covar)
-		xy = self.predict(x, us)
-		wk = mvn( np.zeros(self.num_states), self.x_covar )
-		vk = mvn( np.zeros(self.num_meas), self.y_covar )
-		return xy[0] + wk, xy[1] + vk
-
-	def predict_x_dist (self, xk, Sk, U, cross_cov=False, grad=False):
-		"""
-		Input state posterior mean xk and variance Sk, and controls U 
-			p( x_k | y_{1 : k} ) = N( xk, Sk )
-		Outputs mean and variance of state prediction
-			p( x_{k+1} | y_{1 : k} ) = int[ f(x, u) * N(x | xk, Sk) ] dx
-
-		If U.ndim == 1, one-step prediction.
-		If U.ndim == 2, multi-step prediction
-		"""
-		if U.ndim == 1:
-			return self._predict_x_dist(xk, Sk, U, cross_cov=cross_cov, grad=grad)
-
-		n = len(U)
-		X = np.zeros(( n+1, self.num_states ))
-		S = np.zeros(( n+1, self.num_states, self.num_states ))
-
-		X[0] = xk
-		S[0] = Sk
-		if not grad:
-			for k in range(n):
-				X[k+1], S[k+1] = self._predict_x_dist(X[k], S[k], U[k])
-			return X, S
-
-		do = LatentStateDerivativeObject(self, n=n)
-		for k in range(n):
-			X[k], S[k], dok = self._predict_x_dist(X[k], S[k], U[k], grad=True)
-			do.insert(dok,k)
-		return X, S, do
-
 	def _predict_x_dist (self, xk, Sk, u, cross_cov=False, grad=False):
 		# Implemented in children classes (e.g. LinearModel)
 		raise NotImplementedError
-
-	def predict_z_dist (self, x, s, grad=False):
-		"""
-		Input: latent state mean x and covariance s
-			p( x_k | y_{1 : T} ) = N( x, s )
-		Output: observed state mean and variance of observation
-			p( z_k | y_{1 : T} ) = N( Z, S )
-
-		If x.ndim == 1, one-step prediction
-		If x.ndim == 2, multi-step prediction
-		"""
-		if x.ndim == 1:
-			return self._predict_z_dist(x, s, grad=grad)
-
-		n = len(x)
-		Z = np.zeros(( n, self.num_meas ))
-		S = np.zeros(( n, self.num_meas, self.num_meas ))
-		if not grad:
-			for k in range(n):
-				Z[k], S[k] = self._predict_z_dist(x[k], s[k], grad=grad)
-			return Z, S
-
-		do = MeasDerivativeObject(self, n=n)
-		for k in range(n):
-			Z[k], S[k], dok = self._predict_y_dist(x[k], s[k], grad=True)
-			do.insert(dok,k)
-		return Z, S, do
-
-	def _predict_z_dist (self, x, s, grad=False):
-		Z   = np.matmul(self.H, x)
-		S   = np.matmul(self.H, np.matmul(s, self.H.T) )
-		ret = (Z, S)
-		if grad:
-			do = MeasDerivativeObject(self)
-			do.dMdx = self.H
-			for e1 in range(  self.num_meas ):
-				for e2 in range(  self.num_meas ):
-					do.dSds[e1,e2] = self.H[e1][:,None] * self.H[e2][None,:]
-			ret += (do,)
-		return ret
-
-	def predict_y_dist (self, x, s, grad=False):
-		"""
-		Input: latent state mean x and covariance s
-			p( x_k | y_{1 : T} ) = N( x, s )
-		Output: observed state mean and variance of observation
-			p( y_k | y_{1 : T} ) = N( Y, S )
-
-		If x.ndim == 1, one-step prediction
-		If x.ndim == 2, multi-step prediction
-		"""
-		res = self.predict_z_dist(x, s, grad=grad)
-		if grad:
-			Y, S, do = res
-		else:
-			Y, S = res
-
-		R  = self.y_covar if x.ndim == 1 else self.y_covar[None,:,:]
-		S += R
-
-		if grad:
-			return Y, S, do
-		return Y, S
 
 	def filter (self, yk, x, s):
 		"""
--- doepy/models/statespacemodel.py
+++ doepy/models/statespacemodel.py
@@ -25,7 +25,8 @@ SOFTWARE.
 import numpy as np
 
 from .model import Model
-from ..utils import assert_symmetric_matrix, assert_is_shape
+from . import LatentStateDerivativeObject, MeasDerivativeObject
+from ..utils import assert_symmetric_matrix, assert_is_shape, assert_not_none
 
 class StateSpaceModel (Model):
 	def __init__ (self, candidate_model):
@@ -114,8 +115,156 @@ class StateSpaceModel (Model):
 	"""
 	Function calls
 	"""
-	def predict_x_dist (self, *args, **kwargs):
+	def predict (self, x0, U, **kwargs):
 		"""
-		Return mean and covariance of p( y | f, u, u_covar, p, p_covar )
+		Deterministic prediction from model
+		    x_{k+1} = f( x_k, u_k )
+		        y_k = H * x_k
+
+		If U.ndim == 1, one-step prediction
+		If U.ndim == 2, multi-step prediction
 		"""
+		if x0 is None:
+			x0 = self.x0
+		if self.num_param > 0:
+			assert_not_none(self.p_mean,'%s:p_mean'%self.name)
+
+		if U.ndim == 1:
+			return self._predict(x0, U, **kwargs)
+
+		n = len(U)
+		X = np.zeros(( n+1, self.num_states ))
+		Y = np.zeros((   n, self.num_meas ))
+
+		X[0] = x0
+		for k in range( n ):
+			X[k+1], Y[k] = self._predict(X[k], U[k], **kwargs)
+		return X, Y
+
+	def _predict (self, x0, u, **kwargs):
 		raise NotImplementedError
+
+	def sample (self, x0, U, initial_uncertainty=False):
+		"""
+		Stochastic model simulation
+			x_{k+1} = f( x_k, u_k ) + w_k
+			    y_k = H * x_k + v_k
+
+		If U.ndim == 1, one-step prediction
+		If U.ndim == 2, multi-step prediction
+		"""
+		if x0 is None:
+			x0 = self.x0
+		x0 = x0 if not initial_uncertainty else mvn(x0, self.x0_covar)
+
+		if U.ndim == 1:
+			return self._sample(x0, U)
+
+		n = len(U)
+		X = np.zeros(( n+1, self.num_states ))
+		Y = np.zeros((   n, self.num_meas ))
+
+		X[0] = x0
+		for k in range(n):
+			X[k+1], Y[k] = self._sample(X[k], U[k])
+		return X, Y
+
+	def _sample (self, x, u):
+		us = mvn(u, self.u_covar)
+		xy = self.predict(x, us)
+		wk = mvn( np.zeros(self.num_states), self.x_covar )
+		vk = mvn( np.zeros(self.num_meas), self.y_covar )
+		return xy[0] + wk, xy[1] + vk
+
+	def predict_x_dist (self, xk, Sk, U, cross_cov=False, grad=False):
+		"""
+		Input state posterior mean xk and variance Sk, and controls U 
+			p( x_k | y_{1 : k} ) = N( xk, Sk )
+		Outputs mean and variance of state prediction
+			p( x_{k+1} | y_{1 : k} ) = int[ f(x, u) * N(x | xk, Sk) ] dx
+
+		If U.ndim == 1, one-step prediction.
+		If U.ndim == 2, multi-step prediction
+		"""
+		if U.ndim == 1:
+			return self._predict_x_dist(xk, Sk, U, cross_cov=cross_cov, grad=grad)
+
+		n = len(U)
+		X = np.zeros(( n+1, self.num_states ))
+		S = np.zeros(( n+1, self.num_states, self.num_states ))
+
+		X[0] = xk
+		S[0] = Sk
+		if not grad:
+			for k in range(n):
+				X[k+1], S[k+1] = self._predict_x_dist(X[k], S[k], U[k])
+			return X, S
+
+		do = LatentStateDerivativeObject(self, n=n)
+		for k in range(n):
+			X[k], S[k], dok = self._predict_x_dist(X[k], S[k], U[k], grad=True)
+			do.insert(dok,k)
+		return X, S, do
+
+	def predict_z_dist (self, x, s, grad=False):
+		"""
+		Input: latent state mean x and covariance s
+			p( x_k | y_{1 : T} ) = N( x, s )
+		Output: observed state mean and variance of observation
+			p( z_k | y_{1 : T} ) = N( Z, S )
+
+		If x.ndim == 1, one-step prediction
+		If x.ndim == 2, multi-step prediction
+		"""
+		if x.ndim == 1:
+			return self._predict_z_dist(x, s, grad=grad)
+
+		n = len(x)
+		Z = np.zeros(( n, self.num_meas ))
+		S = np.zeros(( n, self.num_meas, self.num_meas ))
+		if not grad:
+			for k in range(n):
+				Z[k], S[k] = self._predict_z_dist(x[k], s[k], grad=grad)
+			return Z, S
+
+		do = MeasDerivativeObject(self, n=n)
+		for k in range(n):
+			Z[k], S[k], dok = self._predict_y_dist(x[k], s[k], grad=True)
+			do.insert(dok,k)
+		return Z, S, do
+
+	def _predict_z_dist (self, x, s, grad=False):
+		Z   = np.matmul(self.H, x)
+		S   = np.matmul(self.H, np.matmul(s, self.H.T) )
+		ret = (Z, S)
+		if grad:
+			do = MeasDerivativeObject(self)
+			do.dMdx = self.H
+			for e1 in range(  self.num_meas ):
+				for e2 in range(  self.num_meas ):
+					do.dSds[e1,e2] = self.H[e1][:,None] * self.H[e2][None,:]
+			ret += (do,)
+		return ret
+
+	def predict_y_dist (self, x, s, grad=False):
+		"""
+		Input: latent state mean x and covariance s
+			p( x_k | y_{1 : T} ) = N( x, s )
+		Output: observed state mean and variance of observation
+			p( y_k | y_{1 : T} ) = N( Y, S )
+
+		If x.ndim == 1, one-step prediction
+		If x.ndim == 2, multi-step prediction
+		"""
+		res = self.predict_z_dist(x, s, grad=grad)
+		if grad:
+			Y, S, do = res
+		else:
+			Y, S = res
+
+		R  = self.y_covar if x.ndim == 1 else self.y_covar[None,:,:]
+		S += R
+
+		if grad:
+			return Y, S, do
+		return Y, S
