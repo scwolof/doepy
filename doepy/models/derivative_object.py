6abc3241c78fabe8cd11de7ece098f14ab03d834
scwolof <scw.olofsson@gmail.com>
2019-04-08 17:03:06 +0100

    Virtual zero derivatives

--- doepy/models/derivative_object.py
+++ doepy/models/derivative_object.py
@@ -24,65 +24,48 @@ SOFTWARE.
 
 import numpy as np
 
+from ..utils import assert_is_instance, assert_is_shape
+
 class DerivativeObject:
 	def __init__ (self, num_out, num_inputs=None, num_param=None, num_states=None, 
-		Hessian=False, num_test_points=None, covariance=False, 
-		in_out_covariance=False):
+		num_test_points=None):
 
-		has_inputs = num_inputs is not None and num_inputs > 0
-		has_param  = num_param is not None and num_param > 0
-		has_states = num_states is not None and num_states > 0
+		self.has_inputs = num_inputs is not None and num_inputs > 0
+		self.has_param  = num_param is not None and num_param > 0
+		self.has_states = num_states is not None and num_states > 0
 
-		# Outputs
-		i = (num_out,) if num_test_points is None else (num_test_points, num_out)
-		M = np.empty( i )                  # Predictive mean
-		if covariance:
-			S = np.empty( i + (num_out,))  # Predictive covariance
-		if in_out_covariance:
-			V = np.empty( i + (num_out,))  # Predictive input-output covariance
+		# Predictive mean
+		self.M = ( num_out, )
+		if num_test_points is not None:
+			self.M = ( num_test_points, ) + self.M
+		
+		# Predictive covariance
+		self.S = self.M + ( num_out, )
+
+		# Predictive input-output covariance
+		self.V = self.M + ( num_out, )
 
 		# Control input
-		if has_inputs:
-			u = np.empty( num_inputs )
-			self.dMdu = np.zeros( M.shape + u.shape )
-			if covariance:
-				self.dSdu = np.zeros( S.shape + u.shape )
-			if in_out_covariance:
-				self.dVdu = np.zeros( V.shape + u.shape )
-			if Hessian:
-				self.dMduu = np.zeros( M.shape + u.shape + u.shape )
+		if self.has_inputs:
+			self.u  = ( num_inputs, )
+			self.uu = ( num_inputs, num_inputs )
 
-		if has_param:
-			p = np.empty( num_param )              # Model parameter mean
-			self.dMdp = np.zeros( M.shape + p.shape )
-			if covariance:
-				self.dSdp = np.zeros( S.shape + p.shape )
-			if in_out_covariance:
-				self.dVdp = np.zeros( V.shape + p.shape )
-			if Hessian:
-				self.dMdpp = np.zeros( M.shape + p.shape + p.shape )
-				if has_inputs:
-					self.dMdup = np.zeros( M.shape + u.shape + p.shape )
+		# Model parameter
+		if self.has_param:
+			self.p  = ( num_param, )
+			self.pp = ( num_param, )
+			if self.has_inputs:
+				self.up = ( num_inputs, num_param )
 
-		if has_states:
-			x = np.empty( num_states )             # Latent state mean
-			s = np.empty((num_states, num_states)) # Latent state covariance
-
-			self.dMdx = np.zeros( M.shape + x.shape )
-			self.dMds = np.zeros( M.shape + s.shape )
-			if covariance:
-				self.dSdx = np.zeros( S.shape + x.shape )
-				self.dSds = np.zeros( S.shape + s.shape )
-			if in_out_covariance:
-				self.dVdx = np.zeros( V.shape + x.shape )
-				self.dVds = np.zeros( V.shape + s.shape )
-
-			if Hessian:
-				self.dMdxx = np.zeros( M.shape + x.shape + x.shape )
-				if has_inputs:
-					self.dMdxu = np.zeros( M.shape + x.shape + u.shape )
-				if has_param:
-					self.dMdxp = np.zeros( M.shape + x.shape + p.shape )
+		# Latent state mean and covariance
+		if self.has_states:
+			self.x  = ( num_states, ) 
+			self.xx = ( num_states, num_states )
+			self.s  = ( num_states, num_states )
+			if self.has_inputs:
+				self.xu = ( num_states, num_inputs )
+			if self.has_param:
+				self.xp = ( num_states, num_param )
 
 	def insert (self, do, n):
 		D = self.__dict__
@@ -91,23 +74,194 @@ class DerivativeObject:
 				t    = getattr(self,d)
 				t[n] = getattr(do,d)
 
+	def _assert_has_derivative (self, x):
+		if 'u' in x:
+			self._assert_has_given_derivative(self.has_inputs, 'input')
+		if 'p' in x:
+			self._assert_has_given_derivative(self.has_param, 'model parameter')
+		if 'x' in x:
+			self._assert_has_given_derivative(self.has_states, 'latent state')
+
+	def _assert_has_given_derivative (self, attr, name):
+		if not attr:
+			raise AssertionError('Object does not have %s derivatives'%name)
+
+	def _get_shape (self, X, x):
+		return getattr(self, X) + getattr(self, x)
+
+	def _get_derivative (self, X, x):
+		self._assert_has_derivative(x)
+
+		name = '_d%sd%s'%(X,x)
+		if hasattr(self, name):
+			return getattr(self, name)
+		return np.zeros( self._get_shape(X, x) )
+
+	def _set_derivative (self, X, x, value):
+		name = 'd%sd%s'%(X,x)
+		assert_is_instance(value, np.ndarray, name)
+		assert_is_shape(value, self._get_shape(X, x))
+		setattr(self, '_%s'%name, value)
+
+	"""
+	Control input derivatives
+	"""
+	@property
+	def dMdu (self):
+		return self._get_derivative('M', 'u')
+	@dMdu.setter
+	def dMdu (self, value):
+		self._set_derivative('M', 'u', value)
+
+	@property
+	def dSdu (self):
+		return self._get_derivative('S', 'u')
+	@dSdu.setter
+	def dSdu (self, value):
+		self._set_derivative('S', 'u', value)
+
+	@property
+	def dVdu (self):
+		return self._get_derivative('V', 'u')
+	@dVdu.setter
+	def dVdu (self, value):
+		self._set_derivative('V', 'u', value)
+
+	"""
+	Model parameter derivatives
+	"""
+	@property
+	def dMdp (self):
+		return self._get_derivative('M', 'p')
+	@dMdp.setter
+	def dMdp (self, value):
+		self._set_derivative('M', 'p', value)
+
+	@property
+	def dSdp (self):
+		return self._get_derivative('S', 'p')
+	@dSdp.setter
+	def dSdp (self, value):
+		self._set_derivative('S', 'p', value)
+
+	@property
+	def dVdp (self):
+		return self._get_derivative('V', 'p')
+	@dVdp.setter
+	def dVdp (self, value):
+		self._set_derivative('V', 'p', value)
+
+	"""
+	Latent state mean derivatives
+	"""
+	@property
+	def dMdx (self):
+		return self._get_derivative('M', 'x')
+	@dMdx.setter
+	def dMdx (self, value):
+		self._set_derivative('M', 'x', value)
+
+	@property
+	def dSdx (self):
+		return self._get_derivative('S', 'x')
+	@dSdx.setter
+	def dSdx (self, value):
+		self._set_derivative('S', 'x', value)
+
+	@property
+	def dVdx (self):
+		return self._get_derivative('V', 'x')
+	@dVdx.setter
+	def dVdx (self, value):
+		self._set_derivative('V', 'x', value)
+
+	"""
+	Latent state covariance derivatives
+	"""
+	@property
+	def dMds (self):
+		return self._get_derivative('M', 's')
+	@dMds.setter
+	def dMds (self, value):
+		self._set_derivative('M', 's', value)
+
+	@property
+	def dSds (self):
+		return self._get_derivative('S', 's')
+	@dSds.setter
+	def dSds (self, value):
+		self._set_derivative('S', 's', value)
+
+	@property
+	def dVds (self):
+		return self._get_derivative('V', 's')
+	@dVds.setter
+	def dVds (self, value):
+		self._set_derivative('V', 's', value)
+
+	"""
+	Hessians
+	"""
+	@property
+	def dMduu (self):
+		return self._get_derivative('M', 'uu')
+	@dMduu.setter
+	def dMduu (self, value):
+		self._set_derivative('M', 'uu', value)
+
+	@property
+	def dMdpp (self):
+		return self._get_derivative('M', 'pp')
+	@dMdpp.setter
+	def dMdpp (self, value):
+		self._set_derivative('M', 'pp', value)
+
+	@property
+	def dMdxx (self):
+		return self._get_derivative('M', 'xx')
+	@dMdxx.setter
+	def dMdxx (self, value):
+		self._set_derivative('M', 'xx', value)
+
+	@property
+	def dMdup (self):
+		return self._get_derivative('M', 'up')
+	@dMdup.setter
+	def dMdup (self, value):
+		self._set_derivative('M', 'up', value)
+
+	@property
+	def dMdxp (self):
+		return self._get_derivative('M', 'xp')
+	@dMdxp.setter
+	def dMdxp (self, value):
+		self._set_derivative('M', 'xp', value)
+
+	@property
+	def dMdxu (self):
+		return self._get_derivative('M', 'xu')
+	@dMdxu.setter
+	def dMdxu (self, value):
+		self._set_derivative('M', 'xu', value)
+
+
+
 
 class LatentStateDerivativeObject (DerivativeObject):
-	def __init__ (self, model, num_test_points=None, Hessian=False):
+	def __init__ (self, model, num_test_points=None):
 		d = {'num_out': model.num_states,
 		     'num_inputs': model.num_inputs,
 		     'num_param': model.num_param,
 		     'num_states': model.num_states,
-		     'covariance': True,
-		     'in_out_covariance':True,
 		     'num_test_points':num_test_points}
 		super().__init__(**d)
+
+
 
 
 class MeasDerivativeObject (DerivativeObject):
 	def __init__ (self, model, num_test_points=None):
 		d = {'num_out': model.num_meas,
 		     'num_states': model.num_states,
-		     'covariance': True,
 		     'num_test_points':num_test_points}
 		super().__init__(**d)
